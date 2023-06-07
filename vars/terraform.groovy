b6a8b41ecc76e806c916d4d9f371a7bc2b4a8a1c
chandralekhasingasani <chandralekha.s@nutanix.com>
2023-06-07 22:20:05 +0530

    test

--- vars/terraform.groovy
+++ vars/terraform.groovy
@@ -9,7 +9,7 @@ pipeline{
     stages{
         stage('terraform init'){
             steps{
-                sh 'terraform -init -backend-config -var-file=env-${ENV}/backend.tfvars'
+                sh 'terraform init -backend-config -var-file=env-${ENV}/backend.tfvars'
             }
         }
         stage('terraform apply'){
